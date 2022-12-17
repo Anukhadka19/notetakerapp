7dcda68aadf28ad9b5a5589d802ef225a06b3001
Anu Khadka <anuragi.khadka@gmail.com>
2022-12-16 19:56:22 -0500

    new changes

--- routes/notes.js
+++ routes/notes.js
@@ -1,15 +1,15 @@
-const notes = require('express').Router();
-const { router } = require('.');
+const router = require('express').Router();
+
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
 // GET Route for retrieving all the notes
-notes.get('/', (req, res) => {
+router.get('/', (req, res) => {
   readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
 });
 
 // POST Route for a new note
-notes.post('/', (req, res) => {
+router.post('/', (req, res) => {
   console.log(req.body);
 
   const { title, text } = req.body;
@@ -21,7 +21,7 @@ notes.post('/', (req, res) => {
       note_id: uuid(),
     };
 
-    readAndAppend(newNotes, './db/notes.json');
+    readAndAppend(newNote, './db/notes.json');
     res.json(`Note added successfully`);
   } else {
     res.error('Error in adding note');
