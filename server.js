7ca1fbcf3818bb2a9f968ab3042c8c2f565d96c7
Anu Khadka <anuragi.khadka@gmail.com>
2022-12-16 19:51:43 -0500

    moved api

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+
 
 app.use(express.static('public'));
-
+app.use('/api', api);
 // GET Route for notes
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
